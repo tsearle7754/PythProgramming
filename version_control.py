ebcb513bbe04e4a2c3e6db481676508985138771
tambryn.s <tsearle7754@floridapoly.edu>
2025-11-10 19:06:34 -0500

    nov 10

--- version_control.py
+++ version_control.py
@@ -1,22 +1,2 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "1e684e99",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Version control- trace changes in code\n",
-    "# git commands\n"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# Version control- trace changes in code
+    # git commands
